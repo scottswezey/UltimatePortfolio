aa87af1e00a17fd429be85f0eb688dcc5ea341d5
Scott Swezey <129733+scottswezey@users.noreply.github.com>
2020-11-18 14:36:27 -0800

    Added core data helpers and simplified project view with them

---
+++ UltimatePortfolio/Item-CoreDataHelpers.swift
@@ -0,0 +1,27 @@
+//
+//  Item-CoreDataHelpers.swift
+//  UltimatePortfolio
+//
+//  Created by Scott on 11/18/20.
+//
+
+import Foundation
+
+extension Item {
+	var itemTitle: String { title ?? "" }
+	var itemDetail: String { detail ?? "" }
+	var itemCreationDate: Date { creationDate ?? Date() }
+	
+	static var example: Item {
+		let controller = DataController(inMemory: true)
+		let viewContext = controller.container.viewContext
+		
+		let item = Item(context: viewContext)
+		item.title = "Example Item"
+		item.detail = "This is an example item"
+		item.priority = 3
+		item.creationDate = Date()
+		
+		return item
+	}
+}
---
+++ UltimatePortfolio/Project-CoreDataHelpers.swift
@@ -0,0 +1,59 @@
+//
+//  Project-CoreDataHelpers.swift
+//  UltimatePortfolio
+//
+//  Created by Scott on 11/18/20.
+//
+
+import Foundation
+
+extension Project {
+	var projectTitle: String { title ?? "New Project" }
+	var projectDetail: String { detail ?? "" }
+	var projectColor: String { color ?? "Light Blue" }
+	
+	var projectItems: [Item] {
+		let itemsArray = items?.allObjects as? [Item] ?? []
+		
+		return itemsArray.sorted { first, second in
+			if first.completed == false {
+				if second.completed == true {
+					return true
+				}
+			} else if first.completed == true {
+				if second.completed == false {
+					return false
+				}
+			}
+			
+			if first.priority > second.priority {
+				return true
+			} else if first.priority < second.priority {
+				return false
+			}
+			
+			return first.itemCreationDate < second.itemCreationDate
+		}
+	}
+	
+	var completionAmount: Double {
+		let originalItems = items?.allObjects as? [Item] ?? []
+		guard originalItems.isEmpty == false else { return 0 }
+		
+		let completedItems = originalItems.filter(\.completed)
+		return Double(completedItems.count) / Double(originalItems.count)
+	}
+	
+	static var example: Project {
+		let controller = DataController(inMemory: true)
+		let viewContext = controller.container.viewContext
+		
+		let project = Project(context: viewContext)
+		project.title = "Example Project"
+		project.detail = "This is an example project."
+		project.closed = true
+		project.creationDate = Date()
+		
+		return project
+	}
+}
