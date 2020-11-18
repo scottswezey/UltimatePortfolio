588ea687cf1af330d8e2419279f641097f2392ea
Scott Swezey <129733+scottswezey@users.noreply.github.com>
2020-11-18 14:13:35 -0800

    initial commit

--- UltimatePortfolio/ContentView.swift
+++ UltimatePortfolio/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+	var body: some View {
+		TabView {
+			HomeView()
+				.tabItem {
+					Image(systemName: "house")
+					Text("Home")
+				}
+			
+			ProjectsView(showClosedProjects: false)
+				.tabItem {
+					Image(systemName: "list.bullet")
+					Text("Open")
+				}
+			
+			ProjectsView(showClosedProjects: true)
+				.tabItem {
+					Image(systemName: "checkmark")
+					Text("Closed")
+				}
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var dataController = DataController.preview
+	
+	static var previews: some View {
+		ContentView()
+			.environment(\.managedObjectContext, dataController.container.viewContext)
+			.environmentObject(dataController)
+	}
 }
---
+++ UltimatePortfolio/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  UltimatePortfolio
+//
+//  Created by Scott on 11/18/20.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+	@EnvironmentObject var dataController: DataController
+	
+	var body: some View {
+		NavigationView {
+			VStack {
+				Button("Add Data") {
+					dataController.deleteAll()
+					try? dataController.createSampleData()
+				}
+			}
+			.navigationTitle("Home")
+		}
+	}
+}
+
+struct HomeView_Previews: PreviewProvider {
+	static var previews: some View {
+		HomeView()
+	}
+}
