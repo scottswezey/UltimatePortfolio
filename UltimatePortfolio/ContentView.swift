470eb2b22b8a087805fbfe89c3609fe1feb075ef
Scott Swezey <129733+scottswezey@users.noreply.github.com>
2020-11-18 21:41:36 -0800

    First pass of awards views and functionality

--- UltimatePortfolio/ContentView.swift
+++ UltimatePortfolio/ContentView.swift
@@ -32,6 +32,13 @@ struct ContentView: View {
 					Image(systemName: "checkmark")
 					Text("Closed")
 				}
+			
+			AwardsView()
+				.tag(AwardsView.tag)
+				.tabItem {
+					Image(systemName: "rosette")
+					Text("Awards")
+				}
 		}
 	}
 }
